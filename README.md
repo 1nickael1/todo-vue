cb67ebec3238957a46114b90f5620ab5c249e54a
Nickael Vereza Bruzzi <qaznickael@protonmail.com>
2021-12-06 16:00:49 -0300

    first commit

--- README.md
+++ README.md
@@ -1,24 +1,9 @@
-# todo-app-vue
-
-## Project setup
-```
-yarn install
-```
-
-### Compiles and hot-reloads for development
-```
-yarn serve
-```
-
-### Compiles and minifies for production
-```
-yarn build
-```
-
-### Lints and fixes files
-```
-yarn lint
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+- View the optimal layout for the app depending on their device's screen size []
+- See hover states for all interactive elements on the page []
+- Add new todos to the list []
+- Mark todos as complete []
+- Delete todos from the list []
+- Filter by all/active/complete todos []
+- Clear all completed todos []
+- Toggle light and dark mode []
+- **Bonus**: Drag and drop to reorder items on the list []
--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,80 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+	<div class="home-container">
+		<div class="todo-app">
+			<div class="header">
+				<h1>TODO</h1>
+				<img class="icon-sun" src="./assets/images/icon-sun.svg" />
+			</div>
+			<div class="create-todo">
+				<a></a>
+				<input v-model="newTodo" placeholder="Create a new todo..." @keyup.enter="addNewTodo" />
+			</div>
+			<div class="container-todo">
+				<div class="list-todos" v-for="(todo, index) in todos" :key="index">
+					<a @click="checkTodo(index)">
+						<img v-if="todo.completed" src="./assets/images/icon-check.svg" />
+					</a>
+					<p :class="[{'text-checked': todo.completed}]">{{todo.descript}}</p>
+				</div>
+				<div v-if="todos.length > 0" class="list-todos-footer" @click="clearCompleted">
+					<p>Clear Completed</p>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import './assets/Global.css';
+// import TodoCard from './components/TodoCard'
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+	name: 'App',
+	data() {
+			return {
+				todos: [],
+				newTodo: '',
+			}
+	},
+	components: {
+		// TodoCard
+	},
+	mounted() {
+		this.recoveryTodos();
+	},
+	methods: {
+		recoveryTodos() {
+			const todosRecovered =  JSON.parse(localStorage.getItem('@todos'))
+			if(todosRecovered) {
+				this.todos = todosRecovered;
+			}
+		},
+		setOnLocalStorage() {
+			localStorage.setItem('@todos', JSON.stringify(this.todos));
+		},
+		addNewTodo() {
+			this.todos.push({descript: this.newTodo, completed: false});
+			this.newTodo = '';
+
+			this.setOnLocalStorage();
+		},
+		checkTodo(index) {
+			if(this.todos[index].completed) {
+				this.todos[index].completed = false
+			} else {
+				this.todos[index].completed = true
+			}
+
+			this.setOnLocalStorage();
+		},
+		clearCompleted() {
+			this.todos = this.todos.filter(item => item.completed != true);
+			this.setOnLocalStorage();
+		}
+
+	}
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
