c360eba71921d7621a12cccbe457989b860ad326
Nickael Vereza Bruzzi <qaznickael@protonmail.com>
2021-12-07 23:39:37 -0300

    add mobile style, new functions

--- src/App.vue
+++ src/App.vue
@@ -7,72 +7,106 @@
 			</div>
 			<div class="create-todo">
 				<a></a>
-				<input v-model="newTodo" placeholder="Create a new todo..." @keyup.enter="addNewTodo" />
+				<input v-model="newTodo"  placeholder="Create a new todo..." @keyup.enter="addNewTodo" />
 			</div>
 			<div class="container-todo">
-				<div class="list-todos" v-for="(todo, index) in todos" :key="index">
-					<a @click="checkTodo(index)">
-						<img v-if="todo.completed" src="./assets/images/icon-check.svg" />
-					</a>
-					<p :class="[{'text-checked': todo.completed}]">{{todo.descript}}</p>
+				<div class="list-todos" v-for="(todo, index) in todosToShow" :key="index">
+					<div style="width: 100%">
+						<a :class="[{'icon-checked': todo.completed}]" @click="checkTodo(index)">
+							<img v-if="todo.completed" src="./assets/images/icon-check.svg" />
+						</a>
+						<p :class="[{'text-checked': todo.completed}, {'text-unchecked': !todo.completed}]">{{todo.descript}}</p>
+					</div>
+					<img @click="removeTodo(index)" src="./assets/images/icon-cross.svg" alt="">
 				</div>
-				<div v-if="todos.length > 0" class="list-todos-footer" @click="clearCompleted">
-					<p>Clear Completed</p>
+				<div v-if="todosOriginal.length > 0" class="list-todos-footer">
+					<p>{{todosOriginal.length}} items left</p>
+					<div class="buttons-footer">
+						<p @click="listAllTodos" :class="[{'buttons-footer-active': buttonActive === 1}]">All</p>
+						<p @click="listActiveTodos" :class="[{'buttons-footer-active': buttonActive === 2}]">Active</p>
+						<p @click="listCompletedTodos" :class="[{'buttons-footer-active': buttonActive === 3}]">Completed</p>
+					</div>
+					<p @click="clearCompleted">Clear Completed</p>
 				</div>
+			</div>
+			<div class="buttons-footer-mobile">
+				<p @click="listAllTodos" :class="[{'buttons-footer-active': buttonActive === 1}]">All</p>
+				<p @click="listActiveTodos" :class="[{'buttons-footer-active': buttonActive === 2}]">Active</p>
+				<p @click="listCompletedTodos" :class="[{'buttons-footer-active': buttonActive === 3}]">Completed</p>
 			</div>
 		</div>
 	</div>
 </template>
 
 <script>
+import { defineComponent } from '@vue/runtime-core';
 import './assets/Global.css';
 // import TodoCard from './components/TodoCard'
-export default {
-	name: 'App',
-	data() {
-			return {
-				todos: [],
-				newTodo: '',
+export default defineComponent({
+		name: 'App',
+		data() {
+				return {
+					todosOriginal: [],
+					todosToShow: [],
+					newTodo: '',
+					buttonActive: 1,
+				}
+		},
+		mounted() {
+			this.recoveryTodos();
+		},
+		methods: {
+			recoveryTodos() {
+				const todosRecovered =  JSON.parse(localStorage.getItem('@todos'))
+				if(todosRecovered) {
+					this.todosOriginal = todosRecovered;
+					this.todosToShow = todosRecovered;
+				}
+			},
+			setOnLocalStorage() {
+				localStorage.setItem('@todos', JSON.stringify(this.todosOriginal));
+				this.todosToShow = this.todosOriginal
+			},
+			addNewTodo() {
+				this.todosOriginal.push({descript: this.newTodo, completed: false});
+				this.newTodo = '';
+	
+				this.setOnLocalStorage();
+			},
+			checkTodo(index) {
+				if(this.todosOriginal[index].completed) {
+					this.todosOriginal[index].completed = false
+				} else {
+					this.todosOriginal[index].completed = true
+					
+				}
+				this.setOnLocalStorage();
+			},
+			clearCompleted() {
+				this.todosOriginal = this.todosOriginal.filter(item => item.completed != true);
+				this.setOnLocalStorage();
+			},
+			removeTodo(indexTodo) {
+				this.todosOriginal = this.todosOriginal.filter((item, index) => index != indexTodo);
+				this.setOnLocalStorage();
+			},
+			listAllTodos() {
+				this.buttonActive = 1;
+				this.todosToShow =  this.todosOriginal;
+			},
+			listActiveTodos() {
+				this.buttonActive = 2;
+				this.todosToShow = this.todosOriginal.filter(item => item.completed === false);
+			},
+			listCompletedTodos() {
+				this.buttonActive = 3;
+				this.todosToShow = this.todosOriginal.filter(item => item.completed === true);
 			}
-	},
-	components: {
-		// TodoCard
-	},
-	mounted() {
-		this.recoveryTodos();
-	},
-	methods: {
-		recoveryTodos() {
-			const todosRecovered =  JSON.parse(localStorage.getItem('@todos'))
-			if(todosRecovered) {
-				this.todos = todosRecovered;
-			}
-		},
-		setOnLocalStorage() {
-			localStorage.setItem('@todos', JSON.stringify(this.todos));
-		},
-		addNewTodo() {
-			this.todos.push({descript: this.newTodo, completed: false});
-			this.newTodo = '';
-
-			this.setOnLocalStorage();
-		},
-		checkTodo(index) {
-			if(this.todos[index].completed) {
-				this.todos[index].completed = false
-			} else {
-				this.todos[index].completed = true
-			}
-
-			this.setOnLocalStorage();
-		},
-		clearCompleted() {
-			this.todos = this.todos.filter(item => item.completed != true);
-			this.setOnLocalStorage();
+	
 		}
 
-	}
-}
+	}) 
+	
 </script>
 
 <style>
